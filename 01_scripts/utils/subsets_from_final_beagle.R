4622da51a0dca64878e0773014af6bf795277186
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2025-01-22 12:07:48 -0500

    corrected variables

--- 01_scripts/utils/subsets_from_final_beagle.R
+++ 01_scripts/utils/subsets_from_final_beagle.R
@@ -3,17 +3,17 @@
 # 1. Access files ---------------------------------------------------------
 argv <- commandArgs(T)
 BEAGLE_SELEC <- argv[1]
-ITERATIONS <- argv[2]
-SUBSET_SIZE <- argv[3]
+ITERATIONS <- as.numeric(argv[2])
+SUBSET_SIZE <- as.numeric(argv[3])
 
 beagle_selec <- read.delim(BEAGLE_SELEC)
 
 
 # 2. Make N subsets of final SNPs
-for (i in 1:iterations){
-  snp_subset <- beagle_selec[sort(sample(1:nrow(beagle_selec), size = 100, replace = FALSE)), ]
+for (i in 1:ITERATIONS){
+  snp_subset <- beagle_selec[sort(sample(1:nrow(beagle_selec), size = SUBSET_SIZE, replace = FALSE)), ]
   
-  write.table(snp_subset, file = paste0(strsplit(BEAGLE_SELEC, split = '.beagle'), '_subset', i, '.beagle'),
+  write.table(snp_subset, file = paste0(strsplit(BEAGLE_SELEC, split = '.beagle'), '_subset', i, '_', SUBSET_SIZE, 'sites', '.beagle'),
               col.names = sapply(X = colnames(remaining_snp), FUN = function(x) { head(unlist(strsplit(x, split = '.', fixed = TRUE)), 1) }),
               row.names = FALSE, quote = FALSE, sep = "\t")
 }
