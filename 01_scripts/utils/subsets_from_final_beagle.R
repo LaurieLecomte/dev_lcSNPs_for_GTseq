b7503682dee945789c81fa80755a8ad29166421a
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2025-01-22 12:16:12 -0500

    corrected variables

--- 01_scripts/utils/subsets_from_final_beagle.R
+++ 01_scripts/utils/subsets_from_final_beagle.R
@@ -5,6 +5,7 @@ argv <- commandArgs(T)
 BEAGLE_SELEC <- argv[1]
 ITERATIONS <- as.numeric(argv[2])
 SUBSET_SIZE <- as.numeric(argv[3])
+OUT_DIR <- argv[4]
 
 beagle_selec <- read.delim(BEAGLE_SELEC)
 
@@ -13,7 +14,7 @@ beagle_selec <- read.delim(BEAGLE_SELEC)
 for (i in 1:ITERATIONS){
   snp_subset <- beagle_selec[sort(sample(1:nrow(beagle_selec), size = SUBSET_SIZE, replace = FALSE)), ]
   
-  write.table(snp_subset, file = paste0(strsplit(BEAGLE_SELEC, split = '.beagle.gz'), '_subset', i, '_', SUBSET_SIZE, 'sites', '.beagle'),
+  write.table(snp_subset, file = paste0(OUT_DIR, '/', strsplit(basename(BEAGLE_SELEC), split = '.beagle.gz'), '_subset', i, '_', SUBSET_SIZE, 'sites', '.beagle'),
               col.names = sapply(X = colnames(beagle_selec), FUN = function(x) { head(unlist(strsplit(x, split = '.', fixed = TRUE)), 1) }),
               row.names = FALSE, quote = FALSE, sep = "\t")
 }
